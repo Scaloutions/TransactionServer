f2f514029021e7a0d59dd43e11bf55f7cf340310
Daria Sova <todariasova@gmail.com>
2018-01-22 23:54:47 -0800

    Add function example
    Change-Id: Iee0e3e0181079e6cce21f3503fdc9ec4f989f1b5

--- server.go
+++ server.go
@@ -7,15 +7,17 @@ import (
 	"net/http"
 )
 
+func echoString(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi, there!")
+}
+
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
 
-	http.HandleFunc("/getQuote", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi, there!")
-	})
+	http.HandleFunc("/getQuote", echoString)
 
 	log.Fatal(http.ListenAndServe(":9090", nil))
 
