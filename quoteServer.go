722b5d22f6de0d41edb76915ae231916c3726cc4
Daria Sova <todariasova@gmail.com>
2018-01-25 16:14:41 -0800

    Can connect to the Quote Server now

--- quoteServer.go
+++ quoteServer.go
@@ -1,13 +1,17 @@
+
 package main
 
 import (
     "fmt"
-    "net"
-    "bufio"
+	"net"
+//look into how to do requests with bufio
+// might be easier to read ?
+//    "bufio"
 )
 
 const (
-	QUOTE_SERVER_API = "quoteserve.seng"
+	QUOTE_SERVER_API = "quoteserve.seng:4444"
+	//TODO: append port instead
 	PORT = 4444
 	CONNECTION_TYPE = "tcp"
 )
@@ -19,12 +23,34 @@ const (
 
 func connectToQuoteServer(){
 	//this should create a TCP connection with the quote server
+	fmt.Println("Connecting...")
 	conn, err := net.Dial(CONNECTION_TYPE, QUOTE_SERVER_API)
 
 	if err != nil {
 		fmt.Print("Error connecting to the Quote Server: somthing went wrong :(")
 	}
 
-	response, _ := bufio.NewReader(conn).ReadString('\n')
-    fmt.Print("Data received: " + response)
+	fmt.Println("Reading response...")
+
+	//send request here
+	cstr := "oY01WVirLr,S"
+
+	conn.Write([]byte(cstr+"\n"))
+	
+	buff := make([]byte, 1024)
+	len, _ := conn.Read(buff)
+
+	//gives back : 254.69,OY0,S,1516925116307,PXdxruf7H5p9Br19Si5hq+tlsP24mj6hQQbDUZi8v+s=
+	response := string(buff[:len])
+	fmt.Println("Got back: ", response)
+
+	//	response, _ := bufio.NewReader(conn).ReadString('\n')
+	conn.Close()
 }
+
+//main here just for testing
+func main() {
+	connectToQuoteServer()
+	fmt.Println("Check connection now...")	
+}
+
