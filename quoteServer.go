d9cc904b66092772fdc86727e4092084f8483751
Daria Sova <todariasova@gmail.com>
2018-02-05 09:06:32 -0800

    Comment out quoteserver connection mock

--- quoteServer.go
+++ quoteServer.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net"
+	"strconv"
+	"strings"
 
 	"github.com/golang/glog"
 )
@@ -22,50 +24,51 @@ type Quote struct {
 }
 
 func getQuoteFromQS(userid string, stock string) Quote {
-	/*
-		// Get connection to the quote server
-		conn := getConnection()
 
-		cstr := stock+","+userid+"\n"
-		conn.Write([]byte(cstr))
+	// Get connection to the quote server
+	conn := getConnection()
 
-		//TODO: does this have o be 1024 bytes
-		buff := make([]byte, 1024)
-		len, _ := conn.Read(buff)
+	cstr := stock + "," + userid + "\n"
+	conn.Write([]byte(cstr))
 
-		response := string(buff[:len])
-		glog.Info("Got back: ", response)
+	//TODO: does this have o be 1024 bytes
+	buff := make([]byte, 1024)
+	len, _ := conn.Read(buff)
 
-		quoteArgs := strings.Split(response, ",")
+	response := string(buff[:len])
+	glog.Info("Got back: ", response)
 
-		//example response: 254.69,OY0,S,1516925116307,PXdxruf7H5p9Br19Si5hq+tlsP24mj6hQQbDUZi8v+s=
-		// Returns: quote,sym,userid,timestamp,cryptokey\n
-		price, err := strconv.ParseFloat(quoteArgs[0], 64)
-		if err != nil {
-			glog.Error("Cannot parse QS stock price into float64 ", quoteArgs[0])
-		}
+	quoteArgs := strings.Split(response, ",")
 
-		timestamp, err := strconv.ParseInt(quoteArgs[3], 10, 64)
-		if err != nil {
-			glog.Error("Cannot parse QS timestamp into int64 ", quoteArgs[3])
-		}
-
-		return Quote {
-			Price: price,
-			Stock: quoteArgs[1],
-			UserId: quoteArgs[2],
-			Timestamp: timestamp,
-			CryptoKey: quoteArgs[4],
-		}*/
-
-	//this is just to mock quote server response for testing purposes
-	return Quote{
-		Price:     1,
-		Stock:     "S",
-		UserId:    "Agent007",
-		Timestamp: 1516925116307,
-		CryptoKey: "PXdxruf7H5p9Br19Si5hq",
+	// Returns: quote,sym,userid,timestamp,cryptokey
+	price, err := strconv.ParseFloat(quoteArgs[0], 64)
+	if err != nil {
+		glog.Error("Cannot parse QS stock price into float64 ", quoteArgs[0])
 	}
+
+	timestamp, err := strconv.ParseInt(quoteArgs[3], 10, 64)
+	if err != nil {
+		glog.Error("Cannot parse QS timestamp into int64 ", quoteArgs[3])
+	}
+
+	return Quote{
+		Price:     price,
+		Stock:     quoteArgs[1],
+		UserId:    quoteArgs[2],
+		Timestamp: timestamp,
+		CryptoKey: quoteArgs[4],
+	}
+
+	//** Uncomment this to mock QuoteServer hit for local testing **//
+	/*
+		return Quote{
+			Price:     1,
+			Stock:     "S",
+			UserId:    "Agent007",
+			Timestamp: 1516925116307,
+			CryptoKey: "PXdxruf7H5p9Br19Si5hq",
+		}
+	*/
 
 }
 
