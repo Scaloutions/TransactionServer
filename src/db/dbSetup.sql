912b2ac3eb0334458ed7fe99380b3beeb03e6976
Daria Sova <todariasova@gmail.com>
2018-02-07 13:04:04 -0800

    Create accounts and stock tables

--- src/db/dbSetup.sql
+++ src/db/dbSetup.sql
@@ -5,3 +5,19 @@ CREATE TABLE IF NOT EXISTS users (
   user_address      VARCHAR(100),
   user_email        VARCHAR(50)
 );
+
+/*
+  TODO: we want to store dollars and cents separately
+        and set the rigth constraints on numerical fields
+*/
+CREATE TABLE IF NOT EXISTS accounts (
+  account_id        VARCHAR(32) PRIMARY KEY,
+  balance           FLOAT(10,8)
+);
+
+CREATE TABLE IF NOT EXISTS stock (
+  account_id        VARCHAR(32),
+  symbol            VARCHAR(50),
+  amount            FLOAT(10,8),
+  PRIMARY KEY (account_id, symbol)
+)
