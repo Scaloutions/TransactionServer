27b82f98821631b8048bf2699546e7ff608b5e4a
Daria Sova <todariasova@gmail.com>
2018-02-21 12:17:50 -0800

    Add dotenv and read DB credentials from it

--- src/db/databseConnection.go
+++ src/db/databseConnection.go
@@ -4,18 +4,22 @@ import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/golang/glog"
+	"github.com/joho/godotenv"
+	"os"
+	"api"
 )
 
 var (
 	DB *sql.DB
+	DB_NAME string
+	DB_AUTHENTICATION string
 )
 
-const (
-	DB_URL = "localhost://127.0.0.1"
-	DB_NAME = "mysql"
-	DB_USER = "root"
-	DB_PASSWORD = "root"
-)
+// const (
+// 	// DB_URL = "localhost://127.0.0.1"
+// 	DB_NAME = "DAYTRADING"
+// 	DB_AUTHENTICATION = "root:root"
+// )
 
 type User struct {
 	UserId string
@@ -24,12 +28,25 @@ type User struct {
 }
 
 func InitializeDB() {
+	loadCredentials()
 	DB = databaseConnection()
 }
 
+func loadCredentials() {
+	err := godotenv.Load()
+ 	if err != nil {
+ 	   glog.Error("Error loading .env file")
+	}
+	
+	DB_NAME = os.Getenv("DB_NAME")
+	DB_AUTHENTICATION = os.Getenv("DB_USER_NAME") + ":" + os.Getenv("DB_PASSWORD")
+	glog.Info(DB_NAME, " ", DB_AUTHENTICATION)
+}
+
 func databaseConnection() (db *sql.DB) {
-	connectionStr := DB_NAME+":"+DB_PASSWORD+"@tcp("+DB_URL+")"
-	db, err := sql.Open(DB_NAME, connectionStr)
+	// connectionStr := DB_NAME+":"+DB_PASSWORD+"@tcp("+DB_URL+")"
+	// db, err := sql.Open(DB_NAME, connectionStr)
+	db, err := sql.Open("mysql", DB_AUTHENTICATION + "@/" + DB_NAME)
 
 	if err != nil {
 		glog.Error("Failed to establish connection with the Quote Server.")
@@ -41,35 +58,38 @@ func databaseConnection() (db *sql.DB) {
 func Close() {
 	DB.Close()
 }
-/*
-func GetUser(userId string) {
-	user User
-	user.UserId = userId
 
-	res, err DB.Query("SELECT name, account_number FROM users WHERE useId = ?", 
-		userId).Scan(&user.Name, &user.AccountNumber)
+func GetUser(userId string) User {
+	user := User { UserId: userId }
+
+	err := DB.QueryRow("SELECT name, account_number FROM users WHERE useId = ?", userId).Scan(&user.Name, &user.AccountNumber)
 	if err != nil {
 		glog.Error("Can not find the user in the database: ", userId)
-		return nil
+		//TODO: is there a way to return nil here?
 	}
 
-	return User
+	return user
 }
 
-func CreateNewUser(userId string, name string, email string, address string, accId string, uuid string) {
-	stmt, err := DB.Prepare("INSERT users(user_id, user_name, account_number, user_address, user_email)
-				VALUES(?,?,?,?,?)")
+func getAccount(accId string) api.Account {
+	account := api.Account { AccountNumber: accId }
+
+	//TODO: pull from the db
+	return account
+}
+
+func CreateNewUser(userId string, name string, email string, address string, accId string) {
+	stmt, err := DB.Prepare("INSERT users(user_id, user_name, account_number, user_address, user_email) VALUES(?,?,?,?,?)")
 	
 	if err != nil {
-		glog.Erron(err)
+		glog.Error(err)
 		return
 	}
 
-	_, err = stmt.Exec(useId, name, email, address, accId, uuid)
+	_, err = stmt.Exec(userId, name, accId, address, email)
 
 	if err != nil {
 		glog.Error(err)
 		return
 	}
 }
-*/
