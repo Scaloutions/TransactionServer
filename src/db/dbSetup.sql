5408df1a544f93e65b7fb873a13c335ee4e2ef8e
Daria Sova <todariasova@gmail.com>
2018-03-25 22:50:26 -0700

    Username is not unique unique in the db

--- src/db/dbSetup.sql
+++ src/db/dbSetup.sql
@@ -2,7 +2,7 @@ CREATE DATABASE DAYTRADING;
 
 CREATE TABLE IF NOT EXISTS DAYTRADING.users (
   user_id           VARCHAR(32) PRIMARY KEY,
-  user_name         VARCHAR(20) UNIQUE NOT NULL,
+  user_name         VARCHAR(20) NOT NULL,
   -- account_number    VARCHAR(32) UNIQUE NOT NULL,
   user_address      VARCHAR(100),
   user_email        VARCHAR(50)
